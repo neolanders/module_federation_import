587621054b34a2381e6b8a0d33eadba2ad79c0f9
Eric Alter <eric.alter@globalrelay.net>
2022-02-11 10:03:35 -0800

    Add swc-loader to bypass transpiling Js issues and get closer from A10

--- legacyApp/webpack.config.js
+++ legacyApp/webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const deps = require("./package.json").dependencies;
+const devMode = process.env.NODE_ENV === 'development';
+
 module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
@@ -28,15 +30,63 @@ module.exports = {
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules(?!(\/|\\)host)/, //notice transpile of host package, so jsx will work
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-react'] //added this to process jsx in host package
+          test: /\.jsx?$/,
+          exclude: {
+              and: [/node_modules/], // Exclude libraries in node_modules ...
+              not: [
+                  // Except for a few of them that needs to be transpiled because they use modern syntax
+                  /d3/,
+                  /internmap/,
+                  /delaunator/
+              ]
+          },
+          use: devMode ? {
+              loader: "swc-loader",
+              options: {
+                  jsc: {
+                      parser: {
+                          syntax: "ecmascript",
+                          jsx: true
+                      },
+                      target: 'es5'
+                  }
+              }
+          } : {
+              loader: 'babel-loader'
           }
-        },
       },
+      {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: devMode ? {
+              loader: "swc-loader",
+              options: {
+                  jsc: {
+                      parser: {
+                          syntax: "typescript",
+                          tsx: true
+                      },
+                      target: 'es5'
+                  }
+              }
+          } : {
+              loader: 'ts-loader',
+              options: {
+                  // disable type checker - we will use it in fork plugin
+                  transpileOnly: true
+              }
+          }
+      },
+      // {
+      //   test: /\.(ts|tsx|js|jsx)$/,
+      //   exclude: /node_modules(?!(\/|\\)host)/, //notice transpile of host package, so jsx will work
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       presets: ['@babel/preset-react'] //added this to process jsx in host package
+      //     }
+      //   },
+      // },
     ],
   },
 
